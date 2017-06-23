745c8bfd9996bc173aaff075398f9891f63094dc
Jerick26 <jerry.pin26@yahoo.com>
2017-06-23 14:35:15 +0800

    Create thread_pool_waitable_tasks.h

---
+++ basic_class_cpp/thread_pool_waitable_tasks.h
@@ -0,0 +1,51 @@
+#include <utility>
+#include <vector>
+#include <type_traits>
+#include <future>
+#include <thread>
+
+class thread_pool {
+  std::atomic_bool done;
+  threadsafe_queue<function_wrapper> work_queue;
+  std::vector<std::thread> threads;
+  join_threads joiner;
+
+  void worker_thread() {
+    while (!done) {
+      function_wrapper task;
+      if (work_queue.try_pop(task)) {
+        task();
+      } else {
+        std::this_thread::yield();
+      }
+    }
+  }
+
+public:
+  thread_pool() :
+      done(false), joiner(threads) {
+    unsigned int const thread_count = std::thread::hardware_concurrency();
+    try {
+      for (unsigned i=0; i<thread_count; ++i) {
+        threads.push_back(std::thread(&thread_pool::work_thread, this));
+      }
+    } catch (...) {
+      done = true;
+      throw;
+    }
+  }
+  
+  ~thread_pool() {
+    done = true;
+  }
+  
+  template<typename FunctionType>
+  std::future<typename std::result_of<FunctionType()>::type>
+  submit(FunctionType f) {
+    typedef typename std::result_of<FunctionType()>::type result_type;
+    std::packaged_task<result_type()> task(std::move(f));
+    std::future<result_type> res(task.get_future());
+    work_queue.push(std::move(task));
+    return res;
+  }
+};
